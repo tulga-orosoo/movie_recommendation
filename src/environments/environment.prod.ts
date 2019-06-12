355cd2b2c31cb35567b8e214a1a822f780569838
Tulga Orosoo <tulga.orosoo@gmail.com>
2019-06-12 12:37:55 -0500

    inital

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBzZIiQf4fETU6YUT0ElQdP5G3LdUSwoaM",
+    authDomain: "ml-course-project2.firebaseapp.com",
+    databaseURL: "https://ml-course-project2.firebaseio.com",
+    projectId: "ml-course-project2",
+    storageBucket: "ml-course-project2.appspot.com",
+    messagingSenderId: "1091574601113",
+    appId: "1:1091574601113:web:14456e1e2c787541"
+  },
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,17 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBzZIiQf4fETU6YUT0ElQdP5G3LdUSwoaM",
+    authDomain: "ml-course-project2.firebaseapp.com",
+    databaseURL: "https://ml-course-project2.firebaseio.com",
+    projectId: "ml-course-project2",
+    storageBucket: "ml-course-project2.appspot.com",
+    messagingSenderId: "1091574601113",
+    appId: "1:1091574601113:web:14456e1e2c787541"
+  }
+};
